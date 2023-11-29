fa97ab46c775a1549ba0bf22eb35a206fa9ac9a0
CARTOON <brianngugi224@gmail.com>
2023-11-29 23:28:05 +0300

    Task files

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,10 +1,8 @@
 export default function createReportObject(employeesList) {
-    return {
-      allEmployees: employeesList,
-      // eslint-disable-next-line no-shadow
-      getNumberOfDepartments(employeesList) {
-        return Object.keys(employeesList).length;
-      },
-    };
-  }
-  
+  return {
+    allEmployees: employeesList,
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+}
