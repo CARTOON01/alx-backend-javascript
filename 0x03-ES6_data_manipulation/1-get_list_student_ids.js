862a170fb6839489efa62bba57d148cbbd40f799
CARTOON <brianngugi224@gmail.com>
2023-12-05 08:46:32 +0300

    Task 2

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,3 +1,13 @@
+/**
+ * Retrieves ids from a list of students.
+ * @param {{
+*   id: Number,
+*   firstName: String,
+*   location: String
+* }[]} students - The list of students.
+* @author Brian Ngugi
+* @returns
+*/
 export default function get_list_student_ids(students) {
     if (students instanceof Array) {
         return students.map((students) => students.id);
