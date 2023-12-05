06f1b3aa03743e6452ec0b46ee088e0e89c0ed87
CARTOON <brianngugi224@gmail.com>
2023-12-06 00:44:20 +0300

    Tasks

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,16 +1,10 @@
-export const weakMap = new WeakMap();
+const weakMap = new WeakMap();
 
-/**
- * track the usage of the endpoint using a weakMap
- * @param {Object} endpoint
- * @returns {void}
- * @throws {Error} if the endpoint is used more than 5 times
- */
-export function queryAPI(endpoint) {
-  if (weakMap.get(endpoint)) {
-    if (weakMap.get(endpoint) === 4) throw new Error('Endpoint load is high');
-    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-  } else {
-    weakMap.set(endpoint, 1);
-  }
-}
+const queryAPI = (endpoint) => {
+  let total = weakMap.get(endpoint) || 0;
+  weakMap.set(endpoint, total -= -1);
+  if (total >= 5) throw new Error('Endpoint load is high');
+  return total;
+};
+
+export { weakMap, queryAPI };
